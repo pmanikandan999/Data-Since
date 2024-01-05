e5a42013a5940f31740161078f9a84e8abc548bd
pmanikandan999 <55345382+pmanikandan999@users.noreply.github.com>
2024-01-05 21:48:52 +0530

    Add files via upload

---
+++ CentrelTendency/Univariate.py
@@ -0,0 +1,10 @@
+class Univariate():
+    def quanQual(dataset):
+        qual=[]
+        quan=[]
+        for columnName in dataset.columns:
+            if(dataset[columnName].dtypes=='O'):
+                qual.append(columnName)
+            else:
+                quan.append(columnName)
+        return qual,quan
